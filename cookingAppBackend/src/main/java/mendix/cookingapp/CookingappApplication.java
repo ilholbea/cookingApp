d90c62d704ac285415461e936d215e9c236ae437
ilholbea <ilholbea@gmail.com>
2018-10-04 21:20:51 +0300

    First Commit - created Spring Boot app

---
+++ cookingAppBackend/src/main/java/mendix/cookingapp/CookingappApplication.java
@@ -0,0 +1,12 @@
+package mendix.cookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingappApplication.class, args);
+    }
+}
